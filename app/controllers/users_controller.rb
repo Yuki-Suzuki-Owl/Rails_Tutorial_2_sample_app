d399c23ef1c54155ff7a9037f92b5228e5e7e7c0
Yuki Suzuki <tobiuo1994@gmail.com>
2020-07-08 19:45:21 +0900

    break time.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,21 @@
 class UsersController < ApplicationController
   def new
+    # debugger
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(params[:user])
+    if @user.save
+      #
+    else
+      render 'new'
+    end
+  end
+
+  def show
+    @user = User.find_by(id:params[:id])
+    # @user = User.find(params[:id])
+    # debugger
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -10,6 +10,7 @@ Rails.application.routes.draw do
   get '/about',to:'static_pages#about'
   # get 'static_pages/contact'
   get '/contact',to:'static_pages#contact'
+  resources :users
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
