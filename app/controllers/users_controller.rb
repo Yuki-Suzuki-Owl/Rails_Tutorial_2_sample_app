d5a3049d4ef26ef85703c696690fc7f764a4fc9d
Yuki Suzuki <tobiuo1994@gmail.com>
2020-07-09 13:57:41 +0900

    Finish user signup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,9 +5,11 @@ class UsersController < ApplicationController
   end
 
   def create
-    @user = User.new(params[:user])
+    # @user = User.new(params[:user])
+    @user = User.new(user_params)
     if @user.save
-      #
+      flash[:success] = "Welcom to the Sample App!"
+      redirect_to @user
     else
       render 'new'
     end
@@ -18,4 +20,9 @@ class UsersController < ApplicationController
     # @user = User.find(params[:id])
     # debugger
   end
+
+  private
+    def user_params
+      params.require(:user).permit(:name,:email,:password,:password_confirmation)
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   # get 'users/new'
   get '/signup',to:'users#new'
-
+  post '/signup',to:'users#create'
   # get 'static_pages/home'
   root 'static_pages#home'
   # get 'static_pages/help'
