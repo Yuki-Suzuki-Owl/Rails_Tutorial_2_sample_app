4ef544d91089979f7d0909d49a9986959c628943
Yuki Suzuki <tobiuo1994@gmail.com>
2020-07-10 15:21:49 +0900

    break time.

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,18 @@
 Rails.application.routes.draw do
+  root   'static_pages#home'
   # get 'users/new'
-  get '/signup',to:'users#new'
-  post '/signup',to:'users#create'
+  get    '/signup',to:'users#new'
+  post   '/signup',to:'users#create'
   # get 'static_pages/home'
-  root 'static_pages#home'
   # get 'static_pages/help'
-  get '/help',to: 'static_pages#help'
   # get 'static_pages/about'
-  get '/about',to:'static_pages#about'
   # get 'static_pages/contact'
-  get '/contact',to:'static_pages#contact'
+  get    '/help',   to:'static_pages#help'
+  get    '/about',  to:'static_pages#about'
+  get    '/contact',to:'static_pages#contact'
+  get    '/login',   to:'sessions#new'
+  post   '/login',   to:'sessions#create'
+  delete '/logout',  to:'sessions#destroy'
   resources :users
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
