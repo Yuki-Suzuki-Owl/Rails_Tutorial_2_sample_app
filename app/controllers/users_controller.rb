0dce6c235b56eb39ada0e720d7084356813afeb3
Yuki Suzuki <tobiuo1994@gmail.com>
2020-07-16 15:55:57 +0900

    break time.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,7 @@
 class UsersController < ApplicationController
+  before_action :logged_in_user,only:[:edit,:update]
+  before_action :correct_user,only:[:edit,:update]
+
   def new
     # debugger
     @user = User.new
@@ -16,6 +19,20 @@ class UsersController < ApplicationController
     end
   end
 
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find_by(params[:id])
+    if @user.update_attributes(user_params)
+      flash[:success] = "Profile updated"
+      redirect_to @user
+    else
+      render 'edit'
+    end
+  end
+
   def show
     @user = User.find_by(id:params[:id])
     # @user = User.find(params[:id])
@@ -25,5 +42,17 @@ class UsersController < ApplicationController
   private
     def user_params
       params.require(:user).permit(:name,:email,:password,:password_confirmation)
+    end
+
+    def logged_in_user
+      unless logged_in?
+        flash[:danger] = "Please log in."
+        redirect_to login_url
+      end
+    end
+
+    def correct_user
+      @user = User.find_by(params[:id])
+      redirect_to(root_url) unless current_user?(@user)
     end
 end
